af7e4fee9f6425f9a7b18e072d1b7ec1b1a25374
jamie <107353607+jamieeeeeeeeeeeeeeee@users.noreply.github.com>
2023-03-05 10:43:49 +0000

    Add timer

--- Primes/eratosthenes.c
+++ Primes/eratosthenes.c
@@ -4,10 +4,12 @@
 #include <string.h>
 #include <limits.h>
 #include <math.h>
+#include <time.h>
 
 // Sieve of Eratosthenes
 int main(int argc, char **argv) {
-    unsigned long long MAX = 99999999999ULL; // ~10 GB memory
+    clock_t start = clock();
+    unsigned long long MAX = 999999999ULL; // ~10 GB memory
     if (argc == 2) {
         MAX = strtoull(argv[1], NULL, 10);
         if (errno == ERANGE) {
@@ -18,42 +20,46 @@ int main(int argc, char **argv) {
 
     uint8_t *array = malloc((MAX / 8 + 1) * sizeof(uint8_t));
     if (array == NULL) {
-        printf("Error: malloc failed - you don't have enough memory!\n");
+        printf("Error: malloc failed - not enough memory.\n");
         return 1;
     }
-    unsigned long long i = 0;
+
+    uint8_t pow2[8] = {1, 2, 4, 8, 16, 32, 64, 128};
+    
+    unsigned long long i;
     unsigned long long divisor = 1;
     unsigned long long sqrt_max = sqrt(MAX);
 
     memset(array, 255, (MAX / 8 + 1) * sizeof(uint8_t));
 
-    array[0] &= 0xfe;
-    array[1] &= 0xfd;
+    array[0] &= 0xFC; // 0, 1 are not primes
 
     while (divisor < sqrt_max) {
         i = divisor + 1;
         while (i < MAX) {
-            if ((array[i / 8] & (1 << (i % 8))) == 0) {
+            if ((array[i / 8] & (pow2[i % 8])) == 0) {
                 i++;
                 continue;
             }
             break;
         }
         divisor = i;
-        printf("> %llu \r", divisor);
+        //printf("> %llu \r", divisor);
 
         i += divisor;
         while (i < MAX) {
-            array[i / 8] &= ~(1 << (i % 8));
+            array[i / 8] &= ~(pow2[i % 8]);
             i += divisor;
         }
     }
 
-    printf("\033[32m\nSearch completed!\033[0m\n\n");
+    clock_t end = clock();
+    double elapsed = (double)(end - start) * 1000.0 / CLOCKS_PER_SEC;
+    printf("\033[32m\nSearch completed! %0.f ms\033[0m\n\n", elapsed);
     for (i = 0; i < MAX; i++) {
-        if ((array[i / 8] & (1 << (i % 8))) != 0) {
+        if ((array[i / 8] & (pow2[i % 8])) != 0) {
             divisor = i;
-            // printf("\033[32m%llu\033[0m ", divisor);
+            //printf("\033[32m%llu\033[0m ", divisor);
         }
     }
 
